81945bf02137f73c289c66986968fb40cb415873
dhiyarisalah <18220047@std.stei.itb.ac.id>
2023-07-04 14:06:36 +0700

    update chart

--- frontend/src/components/Ganttchart.jsx
+++ frontend/src/components/Ganttchart.jsx
@@ -4,8 +4,8 @@ import { projectData } from '../data/index.js';
 const Ganttchart = () => {
   const [view, setView] = useState('weekly'); // State variable for selected view
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear()); // State variable for selected year
+  const [selectedMonth, setSelectedMonth] = useState(''); // State variable for selected month
 
-  // Generate an array of month names with all the dates
   const months = [
     { name: 'January', dates: Array.from({ length: 31 }, (_, i) => i + 1) },
     { name: 'February', dates: Array.from({ length: 28 }, (_, i) => i + 1) },
@@ -19,14 +19,16 @@ const Ganttchart = () => {
     { name: 'October', dates: Array.from({ length: 31 }, (_, i) => i + 1) },
     { name: 'November', dates: Array.from({ length: 30 }, (_, i) => i + 1) },
     { name: 'December', dates: Array.from({ length: 31 }, (_, i) => i + 1) },
-    // Add more months with their corresponding dates as needed
   ];
 
+  // Year options
   const currentYear = new Date().getFullYear();
   const yearsToShow = 5;
-  const startYear = currentYear + 1;
+  const startYear = currentYear;
+  const endYear = startYear + yearsToShow - 1;
   const yearOptions = Array.from({ length: yearsToShow }, (_, i) => startYear + i);
 
+  // Event handlers
   const handleViewChange = (event) => {
     setView(event.target.value);
   };
@@ -35,99 +37,156 @@ const Ganttchart = () => {
     setSelectedYear(parseInt(event.target.value));
   };
 
+  const handleMonthChange = (event) => {
+    setSelectedMonth(event.target.value);
+  };
+
+  // Helper function to get the relevant dates based on the selected view
   const getDates = (month) => {
-    if (view === 'weekly') {
-      return month.dates.filter((date) => date % 7 === 1);
-    } else {
-      return month.dates;
+    return view === 'weekly' ? [1, 8, 15, 22, 29] : month.dates;
+  };
+
+  // Helper function to get the bullet mark for a specific milestone
+  const getBulletMark = (milestones, selectedYear, month, date) => {
+    // Find the milestone with the matching date
+    const milestone = milestones.find((milestone) => {
+      const milestoneDate = new Date(milestone.date);
+      const milestoneYear = milestoneDate.getFullYear();
+      const milestoneMonth = milestoneDate.getMonth() + 1; // Month value is zero-based
+      const milestoneDay = milestoneDate.getDate();
+
+      return (
+        milestoneYear === selectedYear &&
+        milestoneMonth === months.findIndex((m) => m.name === month) + 1 &&
+        milestoneDay === date
+      );
+    });
+
+    // If no milestone found, return an empty string
+    if (!milestone) return '';
+
+    // Check the selected view and date range to determine whether to display a bullet mark
+    if (view === 'daily') {
+      return '•';
+    } else if (view === 'weekly') {
+      const currentDate = new Date(selectedYear, months.findIndex((m) => m.name === month), date);
+      const startDate = new Date(currentDate);
+      startDate.setDate(startDate.getDate() - ((currentDate.getDay() - 1 + 7) % 7)); // Start from the first day of the week
+      const endDate = new Date(startDate);
+      endDate.setDate(endDate.getDate() + 6); // End on the seventh day of the week
+
+      if (currentDate >= startDate && currentDate <= endDate) {
+        return '•';
+      } else {
+        return '';
+      }
     }
+
+    return '';
   };
 
   return (
-    <div>
-      <div>
-        <label>
-          <input
-            type="radio"
-            value="weekly"
-            checked={view === 'weekly'}
-            onChange={handleViewChange}
-          />
-          Weekly
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="daily"
-            checked={view === 'daily'}
-            onChange={handleViewChange}
-          />
-          Daily
-        </label>
+    <div className="landingpage">
+      <div className="second-row">
+        <div>
+          <label>
+            <input type="radio" value="weekly" checked={view === 'weekly'} onChange={handleViewChange} />
+            Weekly
+          </label>
+          <label>
+            <input type="radio" value="daily" checked={view === 'daily'} onChange={handleViewChange} />
+            Daily
+          </label>
+        </div>
+        {view === 'daily' && (
+          <div>
+            <label htmlFor="year">Select Year:</label>
+            <select id="year" value={selectedYear} onChange={handleYearChange}>
+              {yearOptions.map((year) => (
+                <option key={year} value={year}>
+                  {year}
+                </option>
+              ))}
+            </select>
+            <label htmlFor="month">Select Month:</label>
+            <select id="month" value={selectedMonth} onChange={handleMonthChange}>
+              <option value="">All Months</option>
+              {months.map((month) => (
+                <option key={month.name} value={month.name}>
+                  {month.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th colSpan={months.reduce((acc, month) => acc + getDates(month).length, 0) + 1}>
+                  Year {selectedYear}
+                </th>
+              </tr>
+              <tr>
+                <th rowSpan="2">Projects</th>
+                {months.map((month) => {
+                  if (selectedMonth === '' || selectedMonth === month.name) {
+                    return (
+                      <th colSpan={getDates(month).length} key={month.name}>
+                        {month.name}
+                      </th>
+                    );
+                  }
+                  return null;
+                })}
+              </tr>
+              <tr>
+                {months.map((month) => {
+                  if (selectedMonth === '' || selectedMonth === month.name) {
+                    return getDates(month).map((date) => (
+                      <th key={`${month.name}-${date}`}>{date}</th>
+                    ));
+                  }
+                  return null;
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {projectData.map((project) => (
+                <tr key={project.projectName}>
+                  <td>{project.projectName}</td>
+                  {months.map((month) => {
+                    if (selectedMonth === '' || selectedMonth === month.name) {
+                      return getDates(month).map((date) => (
+                        <td key={`${project.projectName}-${month.name}-${date}`}>
+                          {getBulletMark(project.milestones, selectedYear, month.name, date)}
+                        </td>
+                      ));
+                    }
+                    return null;
+                  })}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div>
-        <label htmlFor="year">Select Year:</label>
-        <select id="year" value={selectedYear} onChange={handleYearChange}>
-          {yearOptions.map((year) => (
-            <option value={year} key={year}>
-              {year}
-            </option>
-          ))}
-        </select>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th colSpan={months.reduce((acc, month) => acc + getDates(month).length, 0) + 1}>
-              Year {selectedYear}
-            </th>
-          </tr>
-          <tr>
-            <th rowSpan="2">Projects</th>
-            {months.map((month) => (
-              <th colSpan={getDates(month).length} key={month.name}>
-                {month.name}
-              </th>
-            ))}
-          </tr>
-          <tr>
-            {months.map((month) =>
-              getDates(month).map((date) => (
-                <th key={`${month.name}-${date}`}>{date}</th>
-              ))
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          {projectData.map((project) => (
-            <tr key={project.projectName}>
-              <td>{project.projectName}</td>
-              {months.map((month) =>
-                getDates(month).map((date) => (
-                  <td key={`${project.projectName}-${month.name}-${date}`}>
-                    {project.milestones.find(
-                      (milestone) =>
-                        milestone.date ===
-                        `${selectedYear}-${months.findIndex((m) => m.name === month.name) + 1}-${date}`
-                    )?.wpName || ''}
-                  </td>
-                ))
-              )}
-            </tr>
-          ))}
-        </tbody>
-      </table>
       <style>
         {`
           table {
             border-collapse: collapse;
           }
-          th, td {
+          th,
+          td {
             border: 1px solid black;
             padding: 8px;
           }
           th[colSpan="4"] {
             text-align: center;
+          }
+          .table-container {
+            max-height: 400px;
+            overflow: auto;
           }
         `}
       </style>
