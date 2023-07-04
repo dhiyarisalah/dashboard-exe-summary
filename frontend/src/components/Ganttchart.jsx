3e9a71988ede8a70022175c06f7df4c0fad9e5b1
dhiyarisalah <18220047@std.stei.itb.ac.id>
2023-07-04 15:07:24 +0700

    update chart

--- frontend/src/components/Ganttchart.jsx
+++ frontend/src/components/Ganttchart.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { projectData } from '../data/index.js';
+import { Dropdown, Table } from 'react-bootstrap';
 
 const Ganttchart = () => {
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear()); // State variable for selected year
@@ -21,7 +22,6 @@ const Ganttchart = () => {
     { name: 'December', weeks: [1, 2, 3, 4, 5], dates: Array.from({ length: 31 }, (_, i) => i + 1) },
   ];
 
-  // Year options
   const currentYear = new Date().getFullYear();
   const yearsToShow = 5;
   const startYear = currentYear;
@@ -40,148 +40,145 @@ const Ganttchart = () => {
     setGranularity(event.target.value);
   };
 
-  // Helper function to get the relevant dates based on the selected view
   const getDates = (month) => {
     return granularity === 'weekly' ? month.weeks : month.dates;
   };
 
-  // Helper function to get the bullet mark for a specific milestone
-// Helper function to get the bullet mark for a specific milestone
-// Helper function to get the bullet mark for a specific milestone
-const getBulletMark = (milestones, selectedYear, month, date) => {
-  // Find the milestone with the matching date
-  const milestone = milestones.find((milestone) => {
-    const milestoneDate = new Date(milestone.date);
-    const milestoneYear = milestoneDate.getFullYear();
-    const milestoneMonth = milestoneDate.getMonth() + 1; // Month value is zero-based
-    const milestoneDay = milestoneDate.getDate();
+  const getBulletMark = (milestones, selectedYear, month, date) => {
+    const milestone = milestones.find((milestone) => {
+      const milestoneDate = new Date(milestone.date);
+      const milestoneYear = milestoneDate.getFullYear();
+      const milestoneMonth = milestoneDate.getMonth() + 1;
+      const milestoneDay = milestoneDate.getDate();
 
-    return (
-      milestoneYear === selectedYear &&
-      milestoneMonth === months.findIndex((m) => m.name === month) + 1 &&
-      milestoneDay === date
-    );
-  });
+      return (
+        milestoneYear === selectedYear &&
+        milestoneMonth === months.findIndex((m) => m.name === month) + 1 &&
+        milestoneDay === date
+      );
+    });
 
-  // If no milestone found, return an empty string
-  if (!milestone) return '';
+    if (!milestone) return '';
 
-  if (granularity === 'weekly') {
-    // Find the first day of the week for the selected date
-    const firstDayOfMonth = new Date(selectedYear, months.findIndex((m) => m.name === month), 1);
-    const firstDayOfWeek = firstDayOfMonth.getDay(); // 0 (Sunday) to 6 (Saturday)
-    
-    // Calculate the week number
-    const weekNumber = Math.ceil((date + firstDayOfWeek) / 7);
-    
-    return `Week ${weekNumber} •`;
-  }
+    if (granularity === 'weekly') {
+      const firstDayOfMonth = new Date(selectedYear, months.findIndex((m) => m.name === month), 1);
+      const firstDayOfWeek = firstDayOfMonth.getDay();
+      const weekNumber = Math.ceil((date + firstDayOfWeek) / 7);
 
-  return '•';
-};
+      return `Week ${weekNumber} •`;
+    }
 
-
+    return '•';
+  };
 
   return (
-    <div className="landingpage">
-      <div className="second-row">
-        <div>
+    <div className="container">
+      <div className="row">
+        <div className="col">
           <label htmlFor="year">Select Year:</label>
-          <select id="year" value={selectedYear} onChange={handleYearChange}>
-            {yearOptions.map((year) => (
-              <option key={year} value={year}>
-                {year}
-              </option>
-            ))}
-          </select>
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="year-dropdown">
+              {selectedYear}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {yearOptions.map((year) => (
+                <Dropdown.Item
+                  key={year}
+                  active={selectedYear === year}
+                  onClick={() => setSelectedYear(year)}
+                >
+                  {year}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className="col">
           <label htmlFor="month">Select Month:</label>
-          <select id="month" value={selectedMonth} onChange={handleMonthChange}>
-            <option value="">All Months</option>
-            {months.map((month) => (
-              <option key={month.name} value={month.name}>
-                {month.name}
-              </option>
-            ))}
-          </select>
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="month-dropdown">
+              {selectedMonth ? selectedMonth : 'All Months'}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item active={!selectedMonth} onClick={() => setSelectedMonth('')}>
+                All Months
+              </Dropdown.Item>
+              {months.map((month) => (
+                <Dropdown.Item
+                  key={month.name}
+                  active={selectedMonth === month.name}
+                  onClick={() => setSelectedMonth(month.name)}
+                >
+                  {month.name}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className="col">
           <label htmlFor="granularity">Select Granularity:</label>
-          <select id="granularity" value={granularity} onChange={handleGranularityChange}>
+          <select
+            id="granularity"
+            className="form-select"
+            value={granularity}
+            onChange={handleGranularityChange}
+          >
             <option value="weekly">Weekly</option>
             <option value="daily">Daily</option>
           </select>
         </div>
-        <div className="table-container">
-          <table>
-            {/* Table header */}
-            <thead>
-              <tr>
-                <th colSpan={months.reduce((acc, month) => acc + getDates(month).length, 0) + 1}>
-                  Year {selectedYear}
-                </th>
-              </tr>
-              <tr>
-                <th rowSpan="2">Projects</th>
+      </div>
+      <div className="table-responsive">
+        <Table bordered>
+          <thead>
+            <tr>
+              <th colSpan={months.reduce((acc, month) => acc + getDates(month).length, 0) + 1}>
+                Year {selectedYear}
+              </th>
+            </tr>
+            <tr>
+              <th rowSpan="2">Projects</th>
+              {months.map((month) => {
+                if (selectedMonth === '' || selectedMonth === month.name) {
+                  return (
+                    <th colSpan={getDates(month).length} key={month.name}>
+                      {month.name}
+                    </th>
+                  );
+                }
+                return null;
+              })}
+            </tr>
+            <tr>
+              {months.map((month) => {
+                if (selectedMonth === '' || selectedMonth === month.name) {
+                  return getDates(month).map((date) => (
+                    <th key={date}>{granularity === 'weekly' ? `W${date}` : date}</th>
+                  ));
+                }
+                return null;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {projectData.map((project) => (
+              <tr key={project.id}>
+                <td>{project.name}</td>
                 {months.map((month) => {
                   if (selectedMonth === '' || selectedMonth === month.name) {
-                    return (
-                      <th colSpan={getDates(month).length} key={month.name}>
-                        {month.name}
-                      </th>
-                    );
-                  }
-                  return null;
-                })}
-              </tr>
-              <tr>
-                {months.map((month) => {
-                  if (selectedMonth === '' || selectedMonth === month.name) {
-                    return getDates(month).map((item) => (
-                      <th key={`${month.name}-${item}`}>{granularity === 'weekly' ? `Week ${item}` : item}</th>
+                    return getDates(month).map((date) => (
+                      <td key={date}>
+                        {getBulletMark(project.milestones, selectedYear, month.name, date)}
+                      </td>
                     ));
                   }
                   return null;
                 })}
               </tr>
-            </thead>
-            {/* Table body */}
-            <tbody>
-              {projectData.map((project) => (
-                <tr key={project.projectName}>
-                  <td>{project.projectName}</td>
-                  {months.map((month) => {
-                    if (selectedMonth === '' || selectedMonth === month.name) {
-                      return getDates(month).map((item) => (
-                        <td key={`${project.projectName}-${month.name}-${item}`}>
-                          {getBulletMark(project.milestones, selectedYear, month.name, item)}
-                        </td>
-                      ));
-                    }
-                    return null;
-                  })}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </Table>
       </div>
-      <style>
-        {`
-          table {
-            border-collapse: collapse;
-          }
-          th,
-          td {
-            border: 1px solid black;
-            padding: 8px;
-          }
-          th[colSpan="4"] {
-            text-align: center;
-          }
-          .table-container {
-            max-height: 400px;
-            overflow: auto;
-          }
-        `}
-      </style>
     </div>
   );
 };
