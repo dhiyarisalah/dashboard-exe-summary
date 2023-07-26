1a11b0a22eb9e92ab06f0235015371bad838a35e
dhiyarisalah <18220047@std.stei.itb.ac.id>
2023-07-26 15:39:00 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage';
-import ProjectDetails from './ProjectDetails';
-import AssigneeDetails from './AssigneeDetails';
-
+import {Routes, Route} from 'react-router-dom'
+import LandingPage from './pages/LandingPage';
+import NavbarComponents from './components/NavbarComponents';
+import ProjectDetails from './pages/ProjectDetails';
+import AssigneeDetails from './pages/AssigneeDetails';
 function App() {
   return (
     <div>
       <NavbarComponents />
       <Routes>
-        <Route path="/nirmala/" element={<LandingPage />} />
-        <Route path="/nirmala/projectdetails/:label" element={<ProjectDetails />} />
-        <Route path="/nirmala/assigneedetails/:label" element={<AssigneeDetails />} />
+        <Route path="/nirmala" Component={LandingPage} />
+        <Route path="/nirmala/projectdetails/:label" Component={ProjectDetails} />
+        <Route path="/nirmala/assigneedetails/:label" Component={AssigneeDetails}/>
       </Routes>
     </div>
   );
 }
-
 
 export default App
